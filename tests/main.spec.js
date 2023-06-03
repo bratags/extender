428a26189eb7bca21f08c154e06c41d5c3660a13
Adrian <adrian@bratags.com>
2023-06-03 10:46:36 -0700

    .

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,14 +1,15 @@
-const { test, expect, basename, appShortName } = require('./fixtures');
+const { test, expect, basename, appShortName } = require('./fixtures')
 
 module.exports = (async () => {
     test.describe(() => {
         test(`popup - ${basename(__filename)} - 1`, async ({ page, context, serviceWorker }) => {
             await test.step('popup should load', async () => {
-                await page.goto(`chrome-extension://${serviceWorker.url().split('/')[2]}/dist/index.html`);
-                await page.bringToFront();
-                await page.waitForSelector('#main');
-                await expect(await page.locator('body')).toContainText(new RegExp(appShortName, 'i'), { useInnerText: true });
-            });
-        });
-    });
-})();
+                await page.goto(`chrome-extension://${serviceWorker.url().split('/')[2]}/dist/index.html`)
+                await new Promise(resolve => setTimeout(resolve, 1000))
+                await page.bringToFront()
+                await page.waitForSelector('#main')
+                await expect(await page.locator('body')).toContainText(new RegExp(appShortName, 'i'), { useInnerText: true })
+            })
+        })
+    })
+})()
