8cf35ab52955ecfe29d8af6ae8879d4f202a56e6
Adrian <adrian@bratags.com>
2023-06-03 10:51:38 -0700

    .

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,8 +5,8 @@ module.exports = (async () => {
         test(`popup - ${basename(__filename)} - 1`, async ({ page, context, serviceWorker }) => {
             await test.step('popup should load', async () => {
                 await page.goto(`chrome-extension://${serviceWorker.url().split('/')[2]}/dist/index.html`)
-                await new Promise(resolve => setTimeout(resolve, 1000))
                 await page.bringToFront()
+                await page.reload()
                 await page.waitForSelector('#main')
                 await expect(await page.locator('body')).toContainText(new RegExp(appShortName, 'i'), { useInnerText: true })
             })
