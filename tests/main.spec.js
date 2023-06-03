acb5587be1d3a848105c0734749d07b9e438662b
Adrian <adrian@bratags.com>
2023-06-03 09:28:37 -0700

    .

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,13 +1,12 @@
-const { spawn } = require('child_process');
-const { test, expect, ids, basename, appShortName } = require('./fixtures');
+const { test, expect, basename, appShortName } = require('./fixtures');
 
 module.exports = (async () => {
     test.describe(() => {
         test(`popup - ${basename(__filename)} - 1`, async ({ page, context, serviceWorker }) => {
             await test.step('popup should load', async () => {
                 await page.goto(`chrome-extension://${serviceWorker.url().split('/')[2]}/dist/index.html`);
-                await new Promise(resolve => setTimeout(resolve, 3000))
                 await page.bringToFront();
+                await page.waitForSelector('body');
                 await expect(page.locator('body')).toContainText(appShortName, { useInnerText: true });
             });
         });
